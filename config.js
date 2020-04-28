c7fb6db0436ebee9bf8f5f16e27040cfc33fc105
Rohit Singh <rohitsingh18997@gmail.com>
2020-04-29 03:28:49 +0530

    trying to connect to cosmodb

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 module.exports = {
     local_url: "mongodb://localhost:27017/dreamt",
-    remote_url: "mongodb://azurecosmosdb-dreamrent:61foWpomBT9pCRTtn8UzuvvvtpnQQ93UDh8M9xoVJibthOHyOmG8uutReXRUOcK6vmKkKC3dUPzXqLoQTN9RoQ==@azurecosmosdb-dreamrent.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@azurecosmosdb-dreamrent@"
 };
